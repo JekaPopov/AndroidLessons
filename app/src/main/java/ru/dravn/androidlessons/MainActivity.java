f034e4c4dea5a5f0ca80b3bc33183fc2958213a8
JekaPopov <jeka_1108@mail.ru>
2018-05-14 11:08:45 +0300

    Clock

--- app/src/main/java/ru/dravn/androidlessons/MainActivity.java
+++ app/src/main/java/ru/dravn/androidlessons/MainActivity.java
@@ -2,6 +2,11 @@ package ru.dravn.androidlessons;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.Time;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView clock = findViewById(R.id.clock);
+
+        Time now = new Time();
+        now.setToNow();
+
+        clock.setText(now.format("%d.%m.%Y %H.%M.%S"));
+
     }
 }
